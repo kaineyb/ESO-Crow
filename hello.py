63c9f3b8e813cef8f51ad803d54ec4e1633746a4
kaineyb <kaineyb@gmail.com>
2020-05-27 16:35:37 +0100

    did some tidying

--- hello.py
+++
@@ -1,6 +0,0 @@
-def hello(name):
-    return '<h1>Hello grandmaster {}</h1>' . format(name)
-
-
-def both_names(name, surname):
-    return '<h1>Hello grandmaster {} {}</h1>' . format(name, surname)
