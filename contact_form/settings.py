22893b121f0608ad826b68d251e2a595f797acf3
Kaine Bruce <kaineyb@gmail.com>
2021-10-19 15:57:54 +0100

    added env var managment
    added keys module

--- contact_form/settings.py
+++ contact_form/settings.py
@@ -2,48 +2,19 @@
 from flask_mail import Mail
 from flask import Flask
 
-import os
-
-
-def keys():
-
-    public = os.environ.get('PUBLIC_KEY')
-    private = os.environ.get('PRIVATE_KEY')
-    email = os.environ.get('EMAIL_ADDRESS')
-    email_pass = os.environ.get('EMAIL_PASSWORD')
-
-    if os.environ.get('LOCAL'):
-        print("  ", "*"*20)
-        print("   Local Environment Keys Loaded")
-        print("  ", "*"*20)
-
-    elif os.environ.get('PRODUCTION'):
-        print("  ", "*"*20)
-        print("   Production Environment Keys Loaded")
-        print("  ", "*"*20)
-
-    else:
-        print("Environment Unknown - Keys Not Loaded!")
-        public, private, email, email_pass = None, None, None, None
-
-    return public, private, email, email_pass
+from keys import EMAIL, EMAIL_PASS, PRIVATE, PUBLIC
 
 
 def init_mail(app: Flask):
     """ Initialises Mail Settings for Flask to use, and recaptcha for the Contact Form"""
-
-    public, private, email, email_pass = keys()
-
-    print("email", email)
-    print("email_pass", email_pass)
 
     mail_settings = {
         "MAIL_SERVER": 'smtp.gmail.com',
         "MAIL_PORT": 465,
         "MAIL_USE_TLS": False,
         "MAIL_USE_SSL": True,
-        "MAIL_USERNAME": email,
-        "MAIL_PASSWORD": email_pass
+        "MAIL_USERNAME": EMAIL,
+        "MAIL_PASSWORD": EMAIL_PASS
     }
 
     app.config.update(mail_settings)
@@ -51,7 +22,7 @@ def init_mail(app: Flask):
     app.config.update(
         RECAPTCHA_OPTIONS={'theme': 'dark'},
 
-        RECAPTCHA_PUBLIC_KEY=public, RECAPTCHA_PRIVATE_KEY=private
+        RECAPTCHA_PUBLIC_KEY=PUBLIC, RECAPTCHA_PRIVATE_KEY=PRIVATE
     )
 
     app.config.from_mapping(
